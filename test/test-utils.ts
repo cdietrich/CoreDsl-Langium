a51bd926b48f3a5defaec6151616d00f31c04aa7
Christian Dietrich <christian.dietrich@itemis.de>
2023-03-03 19:34:44 +0100

    more scoping tests and fixes
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- test/test-utils.ts
+++ test/test-utils.ts
@@ -1,15 +1,17 @@
 import { expect } from "vitest";
-import { AstNode, EmptyFileSystem, LangiumDocument } from 'langium';
-import { parseDocument } from 'langium/test';
+import { AstNode, EmptyFileSystem, LangiumDocument, LangiumServices } from 'langium';
 import { createCoreDslServices } from '../src/language-server/core-dsl-module';
 import { DescriptionContent } from '../src/language-server/generated/ast';
 import { Diagnostic } from "vscode-languageserver";
+import { URI } from 'vscode-uri';
+import { parseDocument } from "langium/test";
 
 const services = createCoreDslServices(EmptyFileSystem).CoreDsl;
 
 export function assertNoErrors(doc: LangiumDocument<AstNode>) {
     assertNoLexerAndParseErrors(doc)
     let diagnostics = doc.diagnostics ?? []
+    console.log(diagnostics.map((d)=>d.message).toString());
     expect(diagnostics).toHaveLength(0);
 }
 
@@ -19,12 +21,43 @@ export function assertErrors(doc: LangiumDocument<AstNode>, test:(x:Diagnostic[]
     test(diagnostics);
 }
 
-export async function parse(modelText: string): Promise<LangiumDocument<AstNode>> {
-    var doc: LangiumDocument<AstNode> = await parseDocument(services, modelText);
-    await services.shared.workspace.DocumentBuilder.build([doc], { validationChecks: 'all' });
+export async function parse(modelText: string, ...others: [URI,string][]): Promise<LangiumDocument<AstNode>> {
+    const metaData = services.LanguageMetaData;
+    const randomNumber = Math.floor(Math.random() * 10000000) + 1000000;
+    const uri = URI.parse(`file:///${randomNumber}${metaData.fileExtensions[0]}`);
+    var doc: LangiumDocument<AstNode> = getDocument(uri, modelText);
+    var otherDocs = others.map((o) => getDocument(o[0],o[1]))
+    let allDocs = [doc, ...otherDocs]
+    await services.shared.workspace.DocumentBuilder.build(allDocs, { validationChecks: 'all' });
     const model = (doc.parseResult.value as DescriptionContent);
     return doc;
 }
+
+export function getDocument(uri: URI, input: string) : LangiumDocument<AstNode> {
+    const document = services.shared.workspace.LangiumDocumentFactory.fromString(input, uri);
+    services.shared.workspace.LangiumDocuments.addDocument(document);
+    return document
+}
+
+export async function parseDocumentWithUri<T extends AstNode = AstNode>(services: LangiumServices, uri: URI, input: string): Promise<LangiumDocument<T>> {
+    const document = await parseHelperWithUri<T>(services)(uri, input);
+    if (!document.parseResult) {
+        throw new Error('Could not parse document');
+    }
+    return document;
+}
+
+export function parseHelperWithUri<T extends AstNode = AstNode>(services: LangiumServices): (uri:URI, input: string) => Promise<LangiumDocument<T>> {
+    const metaData = services.LanguageMetaData;
+    const documentBuilder = services.shared.workspace.DocumentBuilder;
+    return async (uri, input) => {
+        const document = services.shared.workspace.LangiumDocumentFactory.fromString<T>(input, uri);
+        services.shared.workspace.LangiumDocuments.addDocument(document);
+        await documentBuilder.build([document]);
+        return document;
+    };
+}
+
 
 export function assertNoLexerAndParseErrors(result: LangiumDocument<AstNode>) {
     expect(result.parseResult.lexerErrors.length == 0, result.parseResult.lexerErrors.toString()).toBe(true);
