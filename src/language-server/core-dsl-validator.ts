dbfa4c54e7401993bad4d68d52a3bbc118827c7e
Christian Dietrich <christian.dietrich@itemis.de>
2023-03-06 16:02:08 +0100

    implemented some more validations
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- src/language-server/core-dsl-validator.ts
+++ src/language-server/core-dsl-validator.ts
@@ -1,7 +1,8 @@
-import { ValidationAcceptor, ValidationChecks } from 'langium';
-import {  CoreDslAstType, DescriptionContent, Expression, InfixExpression, isCastExpression, isInfixExpression, isPostfixExpression, isPrefixExpression, isPrimaryExpression } from './generated/ast';
+import { AstNode, DiagnosticInfo, ValidationAcceptor, ValidationChecks } from 'langium';
+import { Attribute, FunctionDefinition, Instruction ,Declaration, Declarator, CoreDef, CoreDslAstType, DescriptionContent, Expression, InfixExpression, InstructionSet, isCastExpression, isInfixExpression, isPostfixExpression, isPrefixExpression, isPrimaryExpression } from './generated/ast';
 import type { CoreDslServices } from './core-dsl-module';
 import { TypeProvider } from './core-dsl-typesystem';
+import { integer } from 'vscode-languageclient';
 
 /**
  * Register custom validation checks.
@@ -11,6 +12,12 @@ export function registerValidationChecks(services: CoreDslServices) {
     const validator = services.validation.CoreDslValidator;
     const checks: ValidationChecks<CoreDslAstType> = {
         DescriptionContent: validator.checkPersonStartsWithCapital,
+        CoreDef: validator.checkAttributeNamesISA,
+        InstructionSet: validator.checkAttributeNamesISA,
+        Instruction: validator.checkAttributeNamesInstruction,
+        Declaration: validator.checkAttributeNamesDeclaration,
+        Declarator: validator.checkAttributeNamesDeclarator,
+        FunctionDefinition: validator.checkAttributeNamesFunctionDefinition
         // TODO is currently also disabled in Xtext variant Expression: validator.checkType
     };
     registry.register(checks, validator);
@@ -19,7 +26,62 @@ export function registerValidationChecks(services: CoreDslServices) {
 /**
  * Implementation of custom validations.
  */
+
+type AttributeUsage = 'instruction' | 'function' | 'declaration'
+
+const KnownAttributes = new Map<string, [integer, AttributeUsage]>([
+    ["enable", [1, "instruction"]],
+    ["hls", [0, "instruction"]],
+    ["no_cont", [0, "instruction"]],
+    ["cond", [0, "instruction"]],
+    ["flush", [0, "instruction"]],
+    ["do_not_synthesize", [0, "function"]],
+    ["is_pc", [0, "declaration"]],
+    ["is_main_reg", [0, "declaration"]],
+    ["is_main_mem",[0,  "declaration"]],
+    ["is_interlock_for", [1, "declaration"]],
+    ["clk_budget", [1, "function"]],
+    ["type", [1, "instruction"]]
+])
+
 export class CoreDslValidator {
+
+    checkAttributeNamesISA(isa: CoreDef|InstructionSet, accept: ValidationAcceptor) : void {
+		this.checkAttributes(isa.commonInstructionAttributes, 'instruction', accept, {node: isa, property: 'commonInstructionAttributes'});
+	}
+
+	checkAttributeNamesInstruction(instr: Instruction, accept: ValidationAcceptor) : void {
+		this.checkAttributes(instr.attributes, 'instruction', accept, {node: instr, property: 'attributes'});
+	}
+
+	checkAttributeNamesDeclaration(decl: Declaration, accept: ValidationAcceptor) : void {
+		this.checkAttributes(decl.attributes, 'declaration', accept, {node: decl, property: 'attributes'});
+	}
+
+	checkAttributeNamesDeclarator(decl: Declarator, accept: ValidationAcceptor) : void {
+		this.checkAttributes(decl.attributes, 'declaration', accept, {node: decl, property: 'attributes'});
+	}
+
+	checkAttributeNamesFunctionDefinition(decl: FunctionDefinition, accept: ValidationAcceptor) : void {
+		this.checkAttributes(decl.attributes, 'function', accept, {node: decl, property: 'attributes'});
+	}
+
+    
+
+    checkAttributes<N extends AstNode>(attributes: Attribute[], expectedUsage: AttributeUsage, accept: ValidationAcceptor, feature: DiagnosticInfo<N>): void {
+		for(var attribute of attributes) {
+			let info = KnownAttributes.get(attribute.type);
+			
+			if(info === undefined || info[1] != expectedUsage) {
+                accept('error', "unexpected attribute '" + attribute.type + "'", feature);
+                return;
+            }
+			if(attribute.parameters.length != info[0])
+                accept('error', "attribute '" + attribute.type + "' requires exactly " + info[0] + " parameter(s)", feature);
+		}
+	}
+
+
 
     checkType(e: Expression, accept: ValidationAcceptor): void {
         try {
